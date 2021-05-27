5c013e309078c4c84e38ccecb9faea356a77df69
Brayden <brayden.ogorman@gmail.com>
2021-05-27 11:53:54 +1000

    Updated views for Devise login/signup/edit. Updated validation for listing forms

--- app/models/equipment_listing.rb
+++ app/models/equipment_listing.rb
@@ -7,9 +7,15 @@ class EquipmentListing < ApplicationRecord
   
   validates :photos, limit: { min: 0, max: 5, message: 'Max file limit reached. 5 Files max' }, 
                      content_type: [:png, :jpg, :jpeg]
+
   validates :description, presence: true 
+  validates_length_of :description, maximum: 500
+
   validates :title, presence: true
+  validates_length_of :title, maximum: 60
+
   validates :address, presence: true
+
   validates :price, presence: true
 
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,9 +1,17 @@
 class Service < ApplicationRecord
   belongs_to :user
+  
   validates :title, presence: true
+  validates_length_of :title, maximum: 60
+  
   validates :location, presence: true
+  
   validates :description, presence: true
+  validates_length_of :description, maximum: 500
+
   validates :price, presence: true
+
+  validates :photo, content_type: [:png, :jpg, :jpeg]
 
   has_one_attached :photo
   
